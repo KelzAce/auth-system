74eb9352876d5adc02d4818641efffd5b0deff1e
IK3CHI <ikechigreat@gmail.com>
2024-06-02 17:34:24 +0100

    A minor fix on the app Module

--- src/app.module.ts
+++ src/app.module.ts
@@ -15,7 +15,7 @@ import { User } from './auth/entities/user.entity';
       useFactory: (configService: ConfigService) => ({
         type: 'postgres',
         host: configService.get('DB_HOST'),
-        port: +configService.get<number>('DB_PORT'),
+        port: configService.get<number>('DB_PORT'),
         username: configService.get('DB_USERNAME'),
         password: configService.get('DB_PASSWORD'),
         database: configService.get('DB_NAME'),
